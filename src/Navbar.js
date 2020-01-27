177fd9f844e5cea7b3fccbad988b8a3fa425995e
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-27 22:49:42 +0530

    done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import scoreStoreInstance from './flux/ScoreStore';
 
 const Navbar = () => {
+  let runs = 0;
+  let wickets = 0;
+  let overs = 0;
+
+  const updateScore = () => {
+    runs++;
+    wickets++;
+    overs++;
+    scoreStoreInstance.updateScore(runs, wickets, overs);
+  };
+
   return (
     <React.Fragment>
       <nav className='navbar navbar-dark bg-dark'>
@@ -14,6 +26,10 @@ const Navbar = () => {
           />
           Flux Architecture Demo
         </a>
+
+        <button className='btn btn-primary' onClick={updateScore}>
+          Update
+        </button>
       </nav>
     </React.Fragment>
   );
