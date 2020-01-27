c1d59ae9d76342a53497e3640429caa01261f366
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-27 23:33:38 +0530

    done

--- src/flux/Actions.js
+++ src/flux/Actions.js
@@ -1,4 +1,5 @@
 import scoreDispatcher from './Dispatcher';
+import axios from 'axios';
 
 export const updateScore = (runs, wickets, overs) => {
   scoreDispatcher.dispatch({
@@ -7,4 +8,34 @@ export const updateScore = (runs, wickets, overs) => {
     wickets,
     overs
   });
+};
+
+export const getScoreFromEndpoint = url => {
+  //  dispatch the START_FETCH action
+  scoreDispatcher.dispatch({
+    type: 'START_FETCH'
+  });
+
+  axios
+    .get(url)
+    .then(data => {
+      //  successful. dispatch the FETCH_SCORE_SUCCESS action
+      scoreDispatcher.dispatch({
+        type: 'FETCH_SCORE_SUCCESS',
+        runs: data.runs,
+        wickets: data.wickets,
+        overs: data.overs
+      });
+    })
+    .catch(error => {
+      console.log('Error', error);
+      //  failed. dispatch the FETCH_FAILURE action
+      scoreDispatcher.dispatch({
+        type: 'FETCH_FAILURE'
+      });
+    })
+    .finally(() => {
+      //    fetch cycle completed
+      //  may dispatch action if deem prudent
+    });
 };
--- src/flux/ScoreStore.js
+++ src/flux/ScoreStore.js
@@ -29,9 +29,24 @@ class ScoreStore extends EventEmitter {
   scoreStoreActionHandler(action) {
     //    Usually, actions are objects that essentially command the store
     //  Usual structure: {type: CONSTS_ALL_CAPS, data: ...}
+    switch (action.type) {
+      case 'UPDATE_SCORE':
+        this.updateScore(action.runs, action.wickets, action.overs);
+        break;
+      case 'START_FETCH':
+        console.log('Data fetch starting');
+        break;
+      case 'FETCH_SCORE_SUCCESS':
+        this.updateScore(action.runs, action.wickets, action.overs);
+        break;
+      case 'FETCH_FAILURE':
+        console.log('Data fetch failed (store level)');
+        break;
+      default:
+        break;
+    }
     if (action.type === 'UPDATE_SCORE') {
       //  We may wish to validate here
-      this.updateScore(action.runs, action.wickets, action.overs);
     }
   }
 }
